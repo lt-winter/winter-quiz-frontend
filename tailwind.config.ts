a94bae7690b6a40103b71aff635779fca7479003
lt-winter <minhdong9304@gmail.com>
2026-03-22 18:12:27 +0700

    add tailwind config file

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Đường dẫn đến các file bạn dùng Tailwind
+    "./index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
